2fe213e49a26ddf4131cac7dd88dbad8e5953651
Johan Abraham Jose <johanabraham345@gmail.com>
2026-06-27 13:49:10 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// Keep this file in .gitignore to prevent accidental commits of keys to public repos.
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBVX3pGP02hJTO0zDanZPhIrEp6lq_cw0s",
+    authDomain: "tokyo-table-df75d.firebaseapp.com",
+    projectId: "tokyo-table-df75d",
+    storageBucket: "tokyo-table-df75d.firebasestorage.app",
+    messagingSenderId: "166498379593",
+    appId: "1:166498379593:web:8e9ddbac59a82999129c98"
+};
